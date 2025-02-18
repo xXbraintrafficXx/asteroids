c2f6d1521ad142bd29485585c3526c306bc359db
braintraffic <mandamanda@posteo.net>
2025-02-18 08:35:37 -0600

    shot rate & cooldown added

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self,x,y,radius):
         super().__init__(x, y, radius)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255,255,255), self.triangle(),2)
@@ -25,6 +26,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_cooldown -= dt
 
         if keys[pygame.K_a]: # left
             self.rotate(dt * -1)
@@ -35,16 +37,20 @@ class Player(CircleShape):
         if keys[pygame.K_s]:  # backward
             self.move(dt * -1)
         if keys[pygame.K_SPACE]:  # shoot
-            self.shoot()
+            self.shoot(dt)
+            
 
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    def shoot(self):
-        print("Shooting!")
-        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        bullet.velocity = forward * PLAYER_SHOOT_SPEED
+    def shoot(self, dt):
+        if self.shot_cooldown > 0:
+            return
+        else:    
+            bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            direction = pygame.Vector2(0, 1).rotate(self.rotation)
+            bullet.velocity = direction * PLAYER_SHOOT_SPEED
+        
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN
